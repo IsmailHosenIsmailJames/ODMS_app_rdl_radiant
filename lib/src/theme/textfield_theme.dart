c1932bb2ec1e510d9853c316cdd82054a0549238
MD. Ismail Hosen Ismail James <md.ismailhosenismailjames@gmail.com>
2024-08-21 11:34:36 +0600

    theme sapared for dynamic features

---
+++ lib/src/theme/textfield_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+TextStyle textStyleForField = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+  color: Colors.grey.shade800,
+);
+
+BoxDecoration textFiendBoxDecoration = BoxDecoration(
+  color: Colors.blue.withOpacity(0.2),
+  borderRadius: BorderRadius.circular(8),
+);
