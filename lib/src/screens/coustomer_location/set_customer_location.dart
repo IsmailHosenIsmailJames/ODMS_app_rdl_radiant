5a0581cd6c71eb756400443d42ce9a79d08caaef
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-09-10 21:06:54 +0600

    added infinite scroll functionlatiy

--- lib/src/screens/coustomer_location/set_customer_location.dart
+++ lib/src/screens/coustomer_location/set_customer_location.dart
@@ -5,6 +5,7 @@ import 'package:clipboard/clipboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gap/gap.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:rdl_radiant/src/apis/apis.dart';
 import 'package:rdl_radiant/src/screens/coustomer_location/model/coustomer_list_model.dart';
@@ -20,23 +21,68 @@ class SetCustomerLocation extends StatefulWidget {
 }
 
 class _SetCustomerLocationState extends State<SetCustomerLocation> {
+  ScrollController scrollController = ScrollController();
+  bool isLoadingMore = false;
+  int page = 1;
+  String? searchName;
+  String? searchPartner;
+  int? limit;
+
   @override
   void initState() {
-    loadListOfCoustomer();
+    scrollController.addListener(
+      () {
+        final maxScroll = scrollController.position.maxScrollExtent;
+        final currentScroll = scrollController.position.pixels;
+
+        if (currentScroll == maxScroll) {
+          loadMoreData();
+        }
+      },
+    );
+    loadListOfCoustomer(
+      page: page,
+      searchName: searchName,
+      limit: limit,
+      searchPartner: searchPartner,
+    );
     super.initState();
+  }
+
+  loadMoreData() async {
+    if (isLoadingMore == false) {
+      log("Loading More...");
+      setState(() {
+        isLoadingMore = true;
+      });
+      page++;
+      await loadListOfCoustomer(
+        page: page,
+        searchName: searchName,
+        searchPartner: searchPartner,
+        limit: limit,
+      );
+      setState(() {
+        isLoadingMore = false;
+      });
+    }
   }
 
   List<CoustomerListModel>? customerListModel;
 
-  Future<void> loadListOfCoustomer() async {
+  Future<void> loadListOfCoustomer(
+      {int? page,
+      String? searchName,
+      String? searchPartner,
+      int? limit}) async {
     try {
-      http.Response response =
-          await http.get(Uri.parse(base + getCoustomerList));
+      http.Response response = await http.get(Uri.parse(
+          "$base$getCoustomerList${page == null ? "" : "?page=$page"}${searchName == null ? "" : "&name1=$searchName"}${searchPartner == null ? "" : "&partner=$searchPartner"}${limit == null ? "" : "&limit=$limit"}"));
+      customerListModel = customerListModel ?? [];
       if (response.statusCode == 200) {
         final decodedData = jsonDecode(response.body);
         if (decodedData['success'] == true) {
           List<Map> listOfResult = List<Map>.from(decodedData['results']);
-          customerListModel = [];
           for (Map result in listOfResult) {
             customerListModel!.add(
               CoustomerListModel.fromMap(
@@ -44,9 +90,9 @@ class _SetCustomerLocationState extends State<SetCustomerLocation> {
               ),
             );
           }
-          setState(() {});
         }
       }
+      setState(() {});
     } catch (e) {
       log(e.toString());
     }
@@ -74,108 +120,133 @@ class _SetCustomerLocationState extends State<SetCustomerLocation> {
               ? const Center(
                   child: Text("No Data found"),
                 )
-              : Column(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width,
-                      padding: const EdgeInsets.all(8),
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 247, 244, 244),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 5,
-                              offset: Offset(0, 3)),
-                        ],
-                      ),
-                      child: const CupertinoSearchTextField(),
+              : Column(children: [
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.all(8),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 247, 244, 244),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5,
+                            offset: Offset(0, 3)),
+                      ],
                     ),
-                    Expanded(
-                      child: ListView.builder(
-                        padding: const EdgeInsets.all(10),
-                        itemCount: customerListModel!.length,
-                        itemBuilder: (context, index) {
-                          final current = customerListModel?[index];
-                          if (current == null) return const SizedBox();
-                          return Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.green.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            padding: const EdgeInsets.all(10),
-                            child: Column(
-                              children: [
-                                getRowWidgetForDetailsBox(
-                                    "Partner ID", current.partner),
-                                dividerWhite,
-                                getRowWidgetForDetailsBox(
-                                    "Pharmacy Name", current.name1),
-                                dividerWhite,
-                                getRowWidgetForDetailsBox(
-                                    "Customer Name", current.contactPerson),
-                                dividerWhite,
-                                getRowWidgetForDetailsBox(
-                                  "Coustomer Mobile",
-                                  current.mobileNo,
-                                  optionalWidgetsAtLast: SizedBox(
-                                    height: 23,
-                                    width: 90,
-                                    child: IconButton(
-                                      padding: EdgeInsets.zero,
-                                      onPressed: () {
-                                        FlutterClipboard.copy(
-                                          current.mobileNo ?? "",
-                                        ).then((value) {
-                                          Fluttertoast.showToast(
-                                              msg: current.mobileNo ?? "");
-                                        });
-                                      },
-                                      icon: const Icon(
-                                        Icons.copy,
-                                        size: 17,
-                                      ),
+                    child: const CupertinoSearchTextField(),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      controller: scrollController,
+                      padding: const EdgeInsets.all(10),
+                      itemCount: customerListModel!.length + 1,
+                      itemBuilder: (context, index) {
+                        if (index == customerListModel!.length) {
+                          if (isLoadingMore) {
+                            return Center(
+                              child: LoadingAnimationWidget.threeArchedCircle(
+                                color: Colors.green,
+                                size: 40,
+                              ),
+                            );
+                          }
+                          return null;
+                        }
+                        final current = customerListModel?[index];
+                        if (current == null) return null;
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.green.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              getRowWidgetForDetailsBox(
+                                  "Partner ID", current.partner),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                  "Pharmacy Name", current.name1),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                  "Customer Name", current.contactPerson),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                "Coustomer Mobile",
+                                current.mobileNo,
+                                optionalWidgetsAtLast: SizedBox(
+                                  height: 23,
+                                  width: 90,
+                                  child: IconButton(
+                                    padding: EdgeInsets.zero,
+                                    onPressed: () {
+                                      FlutterClipboard.copy(
+                                        current.mobileNo ?? "",
+                                      ).then((value) {
+                                        Fluttertoast.showToast(
+                                            msg: current.mobileNo ?? "");
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.copy,
+                                      size: 17,
                                     ),
                                   ),
                                 ),
-                                dividerWhite,
+                              ),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                  "Street", current.street),
+                              if ((current.street1 ?? "").isNotEmpty)
+                                const Divider(
+                                  color: Colors.white,
+                                  height: 1,
+                                ),
+                              if ((current.street1 ?? "").isNotEmpty)
                                 getRowWidgetForDetailsBox(
-                                    "Street", current.street),
-                                if ((current.street1 ?? "").isNotEmpty)
-                                  const Divider(
-                                    color: Colors.white,
-                                    height: 1,
+                                    "Street 1", current.street1),
+                              if ((current.street2 ?? "").isNotEmpty)
+                                const Divider(
+                                  color: Colors.white,
+                                  height: 1,
+                                ),
+                              if ((current.street2 ?? "").isNotEmpty)
+                                getRowWidgetForDetailsBox(
+                                    "Street 2", current.street2),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                  "District", current.district),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                  "Upazilla", current.upazilla),
+                              dividerWhite,
+                              getRowWidgetForDetailsBox(
+                                  "Trans. P. zone", current.transPZone),
+                              const Gap(10),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: ElevatedButton.icon(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.location_on,
                                   ),
-                                if ((current.street1 ?? "").isNotEmpty)
-                                  getRowWidgetForDetailsBox(
-                                      "Street 1", current.street1),
-                                if ((current.street2 ?? "").isNotEmpty)
-                                  const Divider(
-                                    color: Colors.white,
-                                    height: 1,
+                                  label: const Text(
+                                    "Set Location",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
                                   ),
-                                if ((current.street2 ?? "").isNotEmpty)
-                                  getRowWidgetForDetailsBox(
-                                      "Street 2", current.street2),
-                                dividerWhite,
-                                getRowWidgetForDetailsBox(
-                                    "District", current.district),
-                                dividerWhite,
-                                getRowWidgetForDetailsBox(
-                                    "Upazilla", current.upazilla),
-                                dividerWhite,
-                                getRowWidgetForDetailsBox(
-                                    "Trans. P. zone", current.transPZone),
-                                dividerWhite,
-                              ],
-                            ),
-                          );
-                        },
-                      ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ]),
     );
   }
 }
