949a42137df163ad934a9e1594fdf5aba8cbce09
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-09-14 23:59:42 +0600

    set destination location and calculate destination and detaitals

--- lib/src/screens/journey/select_journey_end_location.dart
+++ lib/src/screens/journey/select_journey_end_location.dart
@@ -5,11 +5,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:gap/gap.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_places_flutter/google_places_flutter.dart';
 import 'package:google_places_flutter/model/place_type.dart';
 import 'package:google_places_flutter/model/prediction.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:rdl_radiant/src/screens/maps/keys/google_maps_api_key.dart';
 
 class SelectJourneyEndLocation extends StatefulWidget {
@@ -52,8 +54,112 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
   }
 
   FocusNode focusNode = FocusNode();
-  String? description;
+
+  String street = '';
+  String name = '';
+  String administrativeArea = '';
+  String subAdministrativeArea = '';
+  String locality = '';
+  String country = '';
+  String subLocality = '';
+
+  double distance = 0;
   LatLng? destination;
+
+  Future<void> getLocationDetailsFormLatLon(LatLng latlng) async {
+    destination = latlng;
+    markers['destination'] = Marker(
+        markerId: const MarkerId("destination"),
+        position: latlng,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        infoWindow: const InfoWindow(title: "Destination"));
+
+    setState(() {
+      destination = LatLng(latlng.latitude, latlng.longitude);
+    });
+
+    cameraPositionUpdater(
+      destination = LatLng(latlng.latitude, latlng.longitude),
+      zoom: initMyLocation == null
+          ? null
+          : zoomLabelCalculate(
+              destination = LatLng(latlng.latitude, latlng.longitude),
+              LatLng(initMyLocation!.latitude, initMyLocation!.longitude),
+            ),
+    );
+
+    getPoliLinePoints(
+            LatLng(initMyLocation!.latitude, initMyLocation!.longitude),
+            destination = LatLng(latlng.latitude, latlng.longitude))
+        .then(
+      (value) {
+        for (int i = 1; i < value.length; i++) {
+          distance += Geolocator.distanceBetween(value[i - 1].latitude,
+              value[i - 1].longitude, value[i].latitude, value[i].longitude);
+        }
+
+        generatePolylinesFormsPoints(value);
+      },
+    );
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      latlng.latitude,
+      latlng.longitude,
+    );
+    street = '';
+    name = '';
+    administrativeArea = '';
+    subAdministrativeArea = '';
+    locality = '';
+    country = '';
+    subLocality = '';
+    for (Placemark placemark in placemarks) {
+      street +=
+          '${placemark.street ?? ""}${(placemark.street ?? "").isEmpty ? "" : ", "}';
+
+      name +=
+          '${placemark.name ?? ""}${(placemark.name ?? "").isEmpty ? "" : ", "}';
+      if (!administrativeArea.contains(placemark.administrativeArea ?? "")) {
+        administrativeArea +=
+            '${placemark.administrativeArea ?? ""}${(placemark.administrativeArea ?? "").isEmpty ? "" : ", "}';
+      }
+      if (!subAdministrativeArea
+          .contains(placemark.subAdministrativeArea ?? "")) {
+        subAdministrativeArea +=
+            '${placemark.subAdministrativeArea ?? ""}${(placemark.subAdministrativeArea ?? "").isEmpty ? "" : ", "}';
+      }
+      if (!locality.contains(placemark.locality ?? "")) {
+        locality +=
+            '${placemark.locality ?? ""}${(placemark.locality ?? "").isEmpty ? "" : ", "}';
+      }
+      if (!country.contains(placemark.country ?? "")) {
+        country +=
+            '${placemark.country ?? ""}${(placemark.country ?? "").isEmpty ? "" : ", "}';
+      }
+      if (!subLocality.contains(placemark.subLocality ?? "")) {
+        subLocality +=
+            '${placemark.subLocality ?? ""}${(placemark.subLocality ?? "").isEmpty ? "" : ", "}';
+      }
+    }
+
+    if (street.length > 1) street = street.substring(0, street.length - 2);
+    if (name.length > 1) name = name.substring(0, name.length - 2);
+    if (administrativeArea.length > 1) {
+      administrativeArea =
+          administrativeArea.substring(0, administrativeArea.length - 2);
+    }
+    if (subAdministrativeArea.length > 1) {
+      subAdministrativeArea =
+          subAdministrativeArea.substring(0, subAdministrativeArea.length - 2);
+    }
+    if (locality.length > 1) {
+      locality = locality.substring(0, locality.length - 2);
+    }
+    if (country.length > 1) country = country.substring(0, country.length - 2);
+    if (subLocality.length > 1) {
+      subLocality = subLocality.substring(0, subLocality.length - 2);
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,12 +170,20 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
       body: Stack(
         children: [
           initMyLocation == null
-              ? const Center(
-                  child: Text("Loading your location..."),
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      LoadingAnimationWidget.threeRotatingDots(
+                          color: Colors.blue.shade900, size: 30),
+                      const Text("Loading your location..."),
+                    ],
+                  ),
                 )
               : GoogleMap(
-                  onTap: (argument) {
-                    log([argument.latitude, argument.longitude].toString());
+                  onTap: (argument) async {
+                    getLocationDetailsFormLatLon(argument);
                   },
                   zoomControlsEnabled: false,
                   zoomGesturesEnabled: true,
@@ -122,46 +236,12 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
               getPlaceDetailWithLatLng: (Prediction prediction) async {
                 focusNode.unfocus();
                 if (prediction.lat != null && prediction.lng != null) {
-                  log("Getting poliyes");
-
-                  addMarkers(
-                    "des",
-                    LatLng(double.parse(prediction.lat!),
-                        double.parse(prediction.lng!)),
+                  getLocationDetailsFormLatLon(
+                    LatLng(
+                      double.parse(prediction.lat!),
+                      double.parse(prediction.lng!),
+                    ),
                   );
-
-                  setState(() {
-                    description = prediction.description;
-
-                    destination = LatLng(double.parse(prediction.lat!),
-                        double.parse(prediction.lng!));
-                  });
-
-                  await cameraPositionUpdater(
-                    LatLng(double.parse(prediction.lat!),
-                        double.parse(prediction.lng!)),
-                    zoom: initMyLocation == null
-                        ? null
-                        : zoomLabelCalculate(
-                            LatLng(double.parse(prediction.lat!),
-                                double.parse(prediction.lng!)),
-                            LatLng(initMyLocation!.latitude,
-                                initMyLocation!.longitude),
-                          ),
-                  );
-
-                  await getPoliLinePoints(
-                          LatLng(initMyLocation!.latitude,
-                              initMyLocation!.longitude),
-                          LatLng(double.parse(prediction.lat!),
-                              double.parse(prediction.lng!)))
-                      .then(
-                    (value) {
-                      generatePolylinesFormsPoints(value);
-                    },
-                  );
-                } else {
-                  log("Was lat lon null");
                 }
               },
               itemClick: (Prediction prediction) async {
@@ -229,24 +309,17 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          description ?? "",
-                          style: const TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 33,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: const Text("Start"),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      street,
+                      style: const TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "$subLocality, $locality, $subAdministrativeArea, $administrativeArea, $country",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey.shade700),
                     ),
                     Row(
                       children: [
@@ -256,8 +329,8 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const Gap(5),
-                        Text(destination!.latitude.toString()),
+                        const Gap(2),
+                        Text(destination!.latitude.toStringAsFixed(4)),
                         const Gap(15),
                         const Text(
                           "Lon: ",
@@ -265,10 +338,37 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const Gap(5),
-                        Text(destination!.longitude.toString()),
+                        const Gap(2),
+                        Text(destination!.longitude.toStringAsFixed(4)),
                       ],
-                    )
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          "Distance: ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Gap(2),
+                        Text(
+                          "${(distance / 1000).toStringAsFixed(2)} km",
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Gap(10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("Start Journey"),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -296,9 +396,9 @@ class _MyMapViewState extends State<SelectJourneyEndLocation> {
     if (distance > 100) {
       return 11;
     } else if (distance > 50) {
-      return 13;
+      return 12;
     } else {
-      return 15;
+      return 13;
     }
   }
 
