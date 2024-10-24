fe902d47daf89f7e550ca12e96700339519450c1
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-10-24 11:48:21 +0600

    user data and cached will delete if API declared

--- lib/src/core/in_app_update/in_app_android_update/in_app_update_android.dart
+++ lib/src/core/in_app_update/in_app_android_update/in_app_update_android.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:odms/main.dart';
 import 'package:odms/src/core/in_app_update/controller/in_app_update_controller.dart';
 import 'package:odms/src/core/in_app_update/functions/get_device_info.dart';
 import 'package:odms/src/core/in_app_update/functions/get_info_form_api.dart';
@@ -13,53 +14,61 @@ import '../model/latest_app_info.dart';
 
 Future<void> inAppUpdateAndroid(BuildContext context) async {
   final inAppUpdateController = Get.put(InAppUpdateController());
-  LatestAppInfoAPIModel latestAppInfo = await getInfoFormAPI();
-  String? lastVersion = latestAppInfo.version;
-  String? lastBuildNumber = latestAppInfo.buildNumber;
-  if (lastVersion != null && lastBuildNumber != null) {
-    List<String> currentVersionAndBuild = await getAppVersionAndBuildNumber();
-    String currentVersion = currentVersionAndBuild[0];
-    // String currentBuild = currentVersionAndBuild[1];
-    if (compareVersion(
-      currentVersion: currentVersion,
-      latestVersion: lastVersion,
-    )) {
-      final supportedAbisByDevice = await getDeviceSupportedAbisInfo();
-      final supportedAbisByApp =
-          inAppUpdateController.supportedArchitectureList;
-      String? architecture;
-      for (int i = 0; i < supportedAbisByApp.length; i++) {
-        if (supportedAbisByDevice.contains(supportedAbisByApp[i])) {
-          architecture = supportedAbisByApp[i];
-          break;
-        }
-      }
-      String? apkDownloadLink;
-      if (architecture == null) {
-        apkDownloadLink = latestAppInfo.downloadLink;
-      } else {
-        for (int i = 0; i < latestAppInfo.downloadLinkList!.length; i++) {
-          if (latestAppInfo.downloadLinkList![i].architecture == architecture) {
-            apkDownloadLink = latestAppInfo.downloadLinkList![i].link;
+  try {
+    LatestAppInfoAPIModel latestAppInfo = await getInfoFormAPI();
+    String? lastVersion = latestAppInfo.version;
+    String? lastBuildNumber = latestAppInfo.buildNumber;
+    if (lastVersion != null && lastBuildNumber != null) {
+      List<String> currentVersionAndBuild = await getAppVersionAndBuildNumber();
+      String currentVersion = currentVersionAndBuild[0];
+      // String currentBuild = currentVersionAndBuild[1];
+      if (compareVersion(
+        currentVersion: currentVersion,
+        latestVersion: lastVersion,
+      )) {
+        final supportedAbisByDevice = await getDeviceSupportedAbisInfo();
+        final supportedAbisByApp =
+            inAppUpdateController.supportedArchitectureList;
+        String? architecture;
+        for (int i = 0; i < supportedAbisByApp.length; i++) {
+          if (supportedAbisByDevice.contains(supportedAbisByApp[i])) {
+            architecture = supportedAbisByApp[i];
             break;
           }
         }
-      }
-      if (apkDownloadLink != null) {
-        log("Update Available on $apkDownloadLink");
-        await showUpdatePopup(
-          context,
-          latestAppInfoAPIModel: latestAppInfo,
-          currentVersion: currentVersion,
-          apkDownloadLink: apkDownloadLink,
-        );
+        String? apkDownloadLink;
+        if (architecture == null) {
+          apkDownloadLink = latestAppInfo.downloadLink;
+        } else {
+          for (int i = 0; i < latestAppInfo.downloadLinkList!.length; i++) {
+            if (latestAppInfo.downloadLinkList![i].architecture ==
+                architecture) {
+              apkDownloadLink = latestAppInfo.downloadLinkList![i].link;
+              break;
+            }
+          }
+        }
+        if (apkDownloadLink != null) {
+          log("Update Available on $apkDownloadLink");
+          await showUpdatePopup(
+            context,
+            latestAppInfoAPIModel: latestAppInfo,
+            currentVersion: currentVersion,
+            apkDownloadLink: apkDownloadLink,
+          );
+        } else {
+          // ignore
+          log("No Update");
+        }
       } else {
         // ignore
+        isUpdateChecked = true;
         log("No Update");
       }
-    } else {
-      // ignore
-      log("No Update");
     }
+  } catch (e) {
+    // ignore
+    isUpdateChecked = true;
+    log(e.toString());
   }
 }
--- lib/src/core/in_app_update/in_app_android_update/popup_widget.dart
+++ lib/src/core/in_app_update/in_app_android_update/popup_widget.dart
@@ -3,16 +3,17 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:odms/src/core/in_app_update/controller/in_app_update_controller.dart';
 import 'package:odms/src/core/in_app_update/model/latest_app_info.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PopupWidget extends StatefulWidget {
   final LatestAppInfoAPIModel latestAppInfoAPIModel;
@@ -142,56 +143,7 @@ class _PopupWidgetState extends State<PopupWidget> {
                       ElevatedButton(
                         onPressed: () async {
                           if (isDownloading) return;
-                          Directory directory =
-                              await getApplicationCacheDirectory();
-                          String filePath =
-                              '${directory.path}/apk-${widget.latestAppInfoAPIModel.version}.apk';
-
-                          if (widget.isExitsSameVersionAPK == false &&
-                              isDownloaded == false) {
-                            setState(() {
-                              isDownloading = true;
-                            });
-                            Dio().download(
-                              widget.apkDownloadLink,
-                              filePath,
-                              onReceiveProgress: (count, total) {
-                                controllerGetx.downloadProgress.value =
-                                    count / total;
-                              },
-                            ).then((value) {
-                              setState(() {
-                                isDownloaded = true;
-                                isDownloading = false;
-                              });
-                            });
-                          } else if (!(widget.apkInstallPermission ==
-                                  PermissionStatus.granted ||
-                              isGranted)) {
-                            await Permission.requestInstallPackages
-                                .request()
-                                .then(
-                              (value) {
-                                print(value);
-                                if (value == PermissionStatus.granted) {
-                                  setState(() {
-                                    isGranted = true;
-                                  });
-                                }
-                              },
-                            );
-                          } else if ((widget.apkInstallPermission ==
-                                      PermissionStatus.granted ||
-                                  isGranted) &&
-                              (isDownloaded || widget.isExitsSameVersionAPK)) {
-                            final result = await OpenFile.open(filePath);
-
-                            if (result.type != ResultType.done) {
-                              Fluttertoast.showToast(
-                                  msg: "Something went wrong");
-                            }
-                            log(result.message);
-                          }
+                          await onProceedButtonClick();
                         },
                         child: (isDownloaded || widget.isExitsSameVersionAPK)
                             ? (widget.apkInstallPermission ==
@@ -221,5 +173,67 @@ class _PopupWidgetState extends State<PopupWidget> {
         ),
       ),
     );
+  }
+
+  Future<void> onProceedButtonClick() async {
+    Directory directory = await getApplicationCacheDirectory();
+    String filePath =
+        '${directory.path}/apk-${widget.latestAppInfoAPIModel.version}.apk';
+
+    if (widget.isExitsSameVersionAPK == false && isDownloaded == false) {
+      setState(() {
+        isDownloading = true;
+      });
+      Dio().download(
+        widget.apkDownloadLink,
+        filePath,
+        onReceiveProgress: (count, total) {
+          controllerGetx.downloadProgress.value = count / total;
+        },
+      ).then((value) {
+        setState(() {
+          isDownloaded = true;
+          isDownloading = false;
+        });
+      });
+    } else if (!(widget.apkInstallPermission == PermissionStatus.granted ||
+        isGranted)) {
+      await Permission.requestInstallPackages.request().then(
+        (value) {
+          if (value == PermissionStatus.granted) {
+            setState(() {
+              isGranted = true;
+            });
+          }
+        },
+      );
+    } else if ((widget.apkInstallPermission == PermissionStatus.granted ||
+            isGranted) &&
+        (isDownloaded || widget.isExitsSameVersionAPK)) {
+      try {
+        if (widget.latestAppInfoAPIModel.removeCacheAndDataOnUpdate == true) {
+          SharedPreferences info = await SharedPreferences.getInstance();
+          await info.clear();
+          await Hive.box("info").clear();
+        } else {
+          if (widget.latestAppInfoAPIModel.removeCacheOnUpdate == true) {
+            SharedPreferences info = await SharedPreferences.getInstance();
+            await info.clear();
+          }
+          if (widget.latestAppInfoAPIModel.removeDataOnUpdate == true) {
+            await Hive.box("info").clear();
+          }
+        }
+
+        final result = await OpenFile.open(filePath);
+
+        if (result.type != ResultType.done) {
+          Fluttertoast.showToast(msg: "Something went wrong");
+        }
+        log(result.message);
+      } catch (e) {
+        log("Error clearing cache: $e");
+      }
+    }
   }
 }
